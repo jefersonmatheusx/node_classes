04806bdf3f29301779d04c32b34eef7b34cb821b
Jeferson Matheus <jeferson.matheus@brickz.com.br>
2020-08-13 20:30:23 -0300

    add npm

--- readmes/git.mk
+++ readmes/git.mk
@@ -9,4 +9,4 @@
         [alias]
         s = !git status
         c = !git add --all && git commit 
-        l = !git log 
+        l = !git log --pretty=format:'%C(blue)%h%C(red)%d %C(white)%s - %C(cyan)%cn, %C(green)%cr'
